2c5c4cc438ac687c71bb9216cd8dda82e3f9dabf
wadeling <wadeling@gmail.com>
2022-06-06 09:50:23 +0800

    add case

--- drift-prevention-test/entrypoint.sh
+++ drift-prevention-test/entrypoint.sh
@@ -3,5 +3,9 @@
 while true
 do
 	echo "hello world\n"
+	pid=`cut -f 4 -d ' ' /host/proc/self/stat`
+	echo "my pid:$pid\n"
+	res=`ls -l /host/proc/self`
+	echo "res: $res"
 	sleep 10
 done
--- drift-prevention-test/run-docker.sh
+++ drift-prevention-test/run-docker.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker run -d --name dp-test wade23/deploy:dp-test
+#docker run -d --name dp-test wade23/deploy:dp-test
+
+docker run -d --name dp-test -v /proc:/host/proc wade23/deploy:dp-test
